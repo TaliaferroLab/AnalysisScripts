f2325115ca47a2ef8a1270fbfaed454c5c752fc3
Matthew Taliaferro <taliaferrojm@gmail.com>
2020-09-30 15:11:45 -0600

    update cutadapt for MPRA

--- runCutadapt.py
+++ runCutadapt.py
@@ -1,39 +1,41 @@
 #python3
 import subprocess
 
-samples = []
-celllines = ['CAD', 'N2A']
-comps = ['Neurite', 'Soma']
-dox = ['MinusDox', 'PlusDox']
-reps = ['Rep1', 'Rep2', 'Rep3']
-
-for c in celllines:
-	for co in comps:
-		for d in dox:
-			for r in reps:
-				samp = c + '.' + co + '.' + d + '.' + r
-				samples.append(samp)
+samples = ['CAD_Neurite_FF_1_S13_L004_R1_001', 'CAD_Neurite_FF_2_S14_L004_R1_001', 'CAD_Neurite_FF_3_S15_L004_R1_001', 'CAD_Neurite_FF_4_S16_L004_R1_001',
+'CAD_Soma_FF_1_S9_L004_R1_001', 'CAD_Soma_FF_2_S10_L004_R1_001', 'CAD_Soma_FF_3_S11_L004_R1_001', 'CAD_Soma_FF_4_S12_L004_R1_001',
+'N2A_Neurite_FF_1_S5_L004_R1_001', 'N2A_Neurite_FF_2_S6_L004_R1_001', 'N2A_Neurite_FF_3_S7_L004_R1_001', 'N2A_Neurite_FF_4_S8_L004_R1_001',
+'N2A_Soma_FF_1_S1_L004_R1_001', 'N2A_Soma_FF_2_S2_L004_R1_001', 'N2A_Soma_FF_3_S3_L004_R1_001', 'N2A_Soma_FF_4_S4_L004_R1_001',
+'CAD_Neurite_GFP_1_S29_L004_R1_001', 'CAD_Neurite_GFP_2_S30_L004_R1_001', 'CAD_Neurite_GFP_3_S31_L004_R1_001. ', 'CAD_Neurite_GFP_4_S32_L004_R1_001',
+'CAD_Soma_GFP_1_S25_L004_R1_001', 'CAD_Soma_GFP_2_S26_L004_R1_001', 'CAD_Soma_GFP_3_S27_L004_R1_001', 'CAD_Soma_GFP_4_S28_L004_R1_001',
+'N2A_Neurite_GFP_1_S21_L004_R1_001', 'N2A_Neurite_GFP_2_S22_L004_R1_001', 'N2A_Neurite_GFP_3_S23_L004_R1_001', 'N2A_Neurite_GFP_4_S24_L004_R1_001',
+'N2A_Soma_GFP_1_S17_L004_R1_001', 'N2A_Soma_GFP_2_S18_L004_R1_001', 'N2A_Soma_GFP_3_S19_L004_R1_001', 'N2A_Soma_GFP_4_S20_L004_R1_001']
 
 
 #adapter1 = 'GGCGGAAAGATCGCCGTGTAAGTTTGCTTCGATATCCGCATGCTA'
 #adapter2 = 'CCACTTATTTCATGGATACTTGGAATGGTTTCACTAGTGCGACCGCAAGAG'
-adapter1 = 'AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC'
-adapter2 = 'AGATCGGAAGAGCGTCGTGTAGGGAAAGA'
-#adapter3 = 'AAACGACGGCCAGTGAATTGTAATACGACTCACTATAGGCGCTTTTTTTTTT'
-#adapter4 = 'AAAAAAAAAAGCGCCTATAGTGAGTCGTATTACAATTCACTGGCCGTCGTTT'
+adapter1 = 'GGCGGAAAGATCGCCGTGTAAGTTTGCTTCGATATCCGCATGCTA'
+adapter2 = 'CTGATCAGCGGGTTTCACTAGTGCGACCGCAAGAG'
+adapter3 = 'TGGTGGCTGGTGTGGCCAAGCTTCGATATCCGCATGCTA'
+adapter4 = 'GGGAAAAAGATCTCAGTGGTATTTGTGAGCCAGCACTAGTGCGACCGCAAGAG'
 #adapter5 = 'GGCGGAAAGATCGCCGTGTAAGTTTGCTTCGATATCCGCATGCTA'
 #adapter6 = 'CCACTTATTTCATGGATACTTGGAATGGTTTCACTAGTGCGACCGCAAGAG'
 
 
 for idx, sample in enumerate(samples):
 	print('Trimming {0}, sample {1} of {2}...'.format(sample, idx + 1, len(samples)))
-	input1 = '{0}_1.fq.gz'.format(sample)
-	input2 = '{0}_2.fq.gz'.format(sample)
-	output1 = '{0}.R1.trimmed.fq.gz'.format(sample[:-4])
-	output2 = '{0}.R2.trimmed.fq.gz'.format(sample[:-4])
-	statsout = '{0}.cutadaptstats.txt'.format(sample)
-	#command = ['cutadapt', '-g', adapter1, '-G', adapter2, '--minimum-length', '30', '-j', '8',
-	#'-o', output1, '-p', output2, input1, input2]
-	command = ['cutadapt', '-a', adapter1, '-A', adapter2, '--minimum-length', '25', '-j', '8', '-o', output1, '-p', output2, input1, input2]
+	input1 = sample + '.fastq.gz'
+	input2 = input1.replace('_R1_', '_R2_')
+	samplename = sample[:-16]
+	samplename = samplename[:-2] + '_Rep' + samplename[-1]
+	output1 = '{0}.R1.trimmed.fq.gz'.format(samplename)
+	output2 = '{0}.R2.trimmed.fq.gz'.format(samplename)
+	statsout = '{0}.cutadaptstats.txt'.format(samplename)
+	if 'FF' in samplename:
+		adapters = [adapter1, adapter2]
+	elif 'GFP' in samplename:
+		adapters = [adapter3, adapter4]
+	command = ['cutadapt', '-g', adapters[0], '-G', adapters[1], '--minimum-length', '75', '-j', '8',
+	'-o', output1, '-p', output2, input1, input2]
+	#command = ['cutadapt', '-a', adapter1, '-A', adapter2, '--minimum-length', '25', '-j', '8', '-o', output1, '-p', output2, input1, input2]
 	with open(statsout, 'w') as outfh:
 		subprocess.call(command, stdout = outfh)
